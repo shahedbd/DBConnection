9ac5595e87f8b88f34dc918d34c4c4e332366a8d
R M Shahidul Islam Shahed <shahed.mbstu@gmail.com>
2021-10-28 21:55:43 +0600

    + Initial commit

---
+++ DBConnection/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DBConnection.Data;
+using DBConnection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DBConnection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var result = AddCompanyInfo();
+            var GetAllCompanyInfo = _context.CompanyInfo.ToList();
+            return View();
+        }
+
+
+
+        private CompanyInfo AddCompanyInfo()
+        {
+            CompanyInfo _CompanyInfo = new CompanyInfo
+            {
+                Name = "Google",
+                Address = "USA",
+                Email = "infogoogle@gmail.com"
+            };
+
+            _context.Add(_CompanyInfo);
+            _context.SaveChanges();
+
+            return _CompanyInfo;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DBConnection/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DBConnection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnection.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<CompanyInfo> CompanyInfo { get; set; }
+    }
+}
